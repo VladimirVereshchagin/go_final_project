e88621257d3f8cbc1063344fd5869b2670315081
VladimirVereshchagin <vova.vereshagin2016@yandex.ru>
2024-10-15 11:39:32 +0300

    Fixed settings.go and ci.yml

--- tests/settings.go
+++ tests/settings.go
@@ -3,7 +3,14 @@ package tests
 import "os"
 
 var Port = 7540
-var DBFile = "../scheduler.db"
+
+var DBFile = func() string {
+	if envDBFile := os.Getenv("TODO_DBFILE"); envDBFile != "" {
+		return envDBFile
+	}
+	return "../scheduler.db"
+}()
+
 var FullNextDate = true
 var Search = true
 var Token = os.Getenv("TOKEN")
